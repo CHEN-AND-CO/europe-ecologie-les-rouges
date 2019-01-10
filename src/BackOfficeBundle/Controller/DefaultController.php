7e56ec4d9518d1a8327ca6db3b84ad7f1c745df8
Kévin Le Torc'h <kev29lt@gmail.com>
2019-01-10 17:07:52 +0100

    Stats op

--- src/BackOfficeBundle/Controller/DefaultController.php
+++ src/BackOfficeBundle/Controller/DefaultController.php
@@ -6,17 +6,67 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller {
 
+    public function SQLRequest($sqlcode) {
+        $entityManager = $this->getDoctrine()->getManager();
+        $query = $entityManager->createQuery($sqlcode);
+
+        return $query->getResult();
+    }
+
     public function indexAction() {
         return $this->render('BackOfficeBundle:Default:index.html.twig');
     }
 
     public function statsAction() {
-        $entityManager = $this->getDoctrine()->getManager();
-        $query = $entityManager->createQuery(
-                "select ville.ville, count(ville.id) as nb from BackOfficeBundle:Ville ville, BackOfficeBundle:User user where user.ville=ville.id group by ville.id"
+        $nbuserville = $this->SQLRequest(
+                "select ville.ville, count(ville.id) as nb_user from BackOfficeBundle:Ville ville, BackOfficeBundle:User user where user.ville=ville.id group by ville.id"
         );
 
-        return $this->render('BackOfficeBundle:Default:stats.html.twig', array('nbuserville' => $query->getResult()));
+        $nbusersociete = $this->SQLRequest(
+                "select societe.societe, count(societe.id) as nb_user from BackOfficeBundle:Societe societe, BackOfficeBundle:User user where societe.id=user.societe group by societe.id"
+        );
+
+        $nbkmsociete = $this->SQLRequest(
+                "select deplacement.id,"
+                . " deplacement.mois,"
+                . " deplacement.annee,"
+                . " user.id as user_id,"
+                . " societe.societe as societe_name,"
+                . " sum(dJour.nbKm) as nb_km "
+                . "from BackOfficeBundle:Deplacement deplacement,"
+                . " BackOfficeBundle:DeplacementJour dJour,"
+                . " BackOfficeBundle:Societe societe,"
+                . " BackOfficeBundle:User user "
+                . "where deplacement.id=dJour.deplacement and deplacement.user=user.id and user.societe=societe.id "
+                . "group by societe.id "
+                . "order by societe.id "
+        );
+
+        $nbkmuser = $this->SQLRequest(
+                "select deplacement.id,"
+                . " deplacement.mois,"
+                . " deplacement.annee,"
+                . " user.id as user_id,"
+                . " user.nom as user_name,"
+                . " societe.societe as societe_name,"
+                . " sum(dJour.nbKm) as nb_km "
+                . "from BackOfficeBundle:Deplacement deplacement,"
+                . " BackOfficeBundle:DeplacementJour dJour,"
+                . " BackOfficeBundle:Societe societe,"
+                . " BackOfficeBundle:User user "
+                . "where deplacement.id=dJour.deplacement and deplacement.annee=".date("Y")." and deplacement.user=user.id and user.societe=societe.id "
+                . "group by user.id"
+        );
+
+        return $this->render(
+                        'BackOfficeBundle:Default:stats.html.twig',
+                        array(
+                            'nbuserville' => $nbuserville,
+                            'nbusersociete' => $nbusersociete,
+                            'nbkmsociete' => $nbkmsociete,
+                            'nbkmuser' => $nbkmuser
+                        )
+        );
     }
 
 }
