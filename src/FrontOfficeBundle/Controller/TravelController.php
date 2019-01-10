b17e6e85d42bcb40eec2038e570712d2a6fbc03c
azeaza <alexandre.moigne@hotmail.fr>
2019-01-10 17:08:04 +0100

    test

--- src/FrontOfficeBundle/Controller/TravelController.php
+++ src/FrontOfficeBundle/Controller/TravelController.php
@@ -9,7 +9,16 @@ class TravelController extends Controller
 
     public function indexAction()
     {
-        return $this->render('FrontOfficeBundle:Travel:list.html.twig');
+        $entityManager = $this->getDoctrine()->getManager();
+        $query = $entityManager->createQuery(
+                "select deplacementjour.id, deplacementjour.jour,deplacement.mois, deplacement.annee, deplacementjour.nbKm, typedeplacement.typeDeplacement from BackOfficeBundle:DeplacementJour deplacementjour join BackOfficeBundle:deplacement deplacement on deplacementjour.deplacement=deplacement.id join BackOfficeBundle:TypeDeplacement typedeplacement on deplacementjour.typeDeplacement=typedeplacement.id"
+        );
+
+        //$travels = $this->getDoctrine()->getRepository("BackOfficeBundle:Deplacement")->findAll();
+
+        return $this->render('FrontOfficeBundle:Travel:list.html.twig', array(
+            'travels' => $query->getResult()
+        ));
     }
 
     public function createAction()
